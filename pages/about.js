36d7b4ca301a2629640ab2056744c72de0677f62
doqax <collier.dorian@pm.me>
2022-11-26 23:51:28 +0100

    border on image

--- pages/about.js
+++ pages/about.js
@@ -2,22 +2,22 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "styles/about.module.css";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
-
 import { BsTwitter } from "react-icons/bs";
-
 import ImprovedLink from "components/Article/ImprovedLink/ImprovedLink";
 
 export default function About() {
   return (
     <section className={styles.introduction}>
       <div className={styles.image}>
-        <Image
-          src="/images/me_cut.jpg"
-          alt="Picture of Dorian"
-          width={142}
-          height={150}
-          style={{ borderRadius: "100%" }}
-        />
+        <div className={styles.corner}>
+          <Image
+            src="/images/me_cut.jpg"
+            alt="Picture of Dorian"
+            width={142}
+            height={150}
+            style={{ borderRadius: "100%" }}
+          />
+        </div>
       </div>
       <p className={styles.text}>
         Hi. I'm Dorian, just a developer. I live in Charleroi, Belgium. Here's
